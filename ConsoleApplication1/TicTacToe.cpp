34738c76fe5b4469c481e8f2a8a3229ce44210e3
Thalente Ngcobo <mrti.ngcobo@yahoo.com>
2016-03-16 12:44:13 +0200

    added a reset method

--- ConsoleApplication1/TicTacToe.cpp
+++ ConsoleApplication1/TicTacToe.cpp
@@ -39,3 +39,13 @@ void TicTacToe ::  print()
 
 }
 
+void TicTacToe::reset(){
+
+	int row, col;
+	for (row = 0; row < 3; row++)
+	{
+		for (col = 0; col < 3; col++)
+		{
+			matrix[row][col] = { '0' };
+		}
+	}
